69e48511f00dcd0636a86928315c583f14e4235d
Alicia Tsai <alicitsai@ocf.berkeley.edu>
2019-10-01 13:58:23 -0700

    fdsfdsf

--- b3/phonebook_skeleton.sh
+++ b3/phonebook_skeleton.sh
@@ -7,21 +7,21 @@ if [ "$#" -lt 1 ]; then
     exit 1
 
 elif [ "$1" = "new" ]; then
-    # YOUR CODE HERE #
+    echo "$2: $3" >> $PHONEBOOK_ENTRIES
 
 elif [ "$1" = "list" ]; then
     if [ ! -e $PHONEBOOK_ENTRIES ] || [ ! -s $PHONEBOOK_ENTRIES ]; then
         echo "phonebook is empty"
     else
-        # YOUR CODE HERE #
+        cat $PHONEBOOK_ENTRIES
     fi
 
 elif [ "$1" = "remove" ]; then
-    # YOUR CODE HERE #
+    sed -i "$2" $PHONEBOOK_ENTRIES
 
 elif [ "$1" = "clear" ]; then
-    # YOUR CODE HERE #
+    cp /dev/null $PHONEBOOK_ENTRIES
 
 else
-     # YOUR CODE HERE #
+     grep "$1"
 fi
