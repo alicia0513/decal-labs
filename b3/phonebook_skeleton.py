8d55d9099bb86ec45dc35192279b7b7a950313dd
Alicia Tsai <alicitsai@ocf.berkeley.edu>
2019-10-01 13:35:22 -0700

    finished python

--- b3/phonebook_skeleton.py
+++ b3/phonebook_skeleton.py
@@ -5,34 +5,46 @@ import os
 
 PHONEBOOK_ENTRIES = "python_phonebook_entries"
 
-
 def main():
     if len(sys.argv) < 2:
         exit(1)
 
     elif sys.argv[1] == "new":
-        # YOUR CODE HERE #
+        name = sys.argv[2]
+        number = sys.argv[3]
+        with open(PHONEBOOK_ENTRIES, 'a+') as f:
+            f.write(name + ": " + number + "\n")
 
     elif sys.argv[1] == "list":
         if not os.path.isfile(PHONEBOOK_ENTRIES) or os.path.getsize(
                 PHONEBOOK_ENTRIES) == 0:
             print("phonebook is empty")
         else:
-            # YOUR CODE HERE #
+            with open(PHONEBOOK_ENTRIES, 'r') as f:
+                for line in f.readlines():
+                    print line,
 
     elif sys.argv[1] == "remove":
         name = " ".join(sys.argv[2:])
-        # YOUR CODE HERE #
+        with open(PHONEBOOK_ENTRIES, 'r') as f:
+            lines = f.readlines()
+        with open(PHONEBOOK_ENTRIES, 'w') as f:
+            for line in lines:
+                entry = line.split(":")
+                if entry[0] != name:
+                    f.write(line)
 
     elif sys.argv[1] == "clear":
-        # YOUR CODE HERE #
+        with open(PHONEBOOK_ENTRIES, 'w') as f:
+            f.close()
 
     else:
         name = " ".join(sys.argv[1:])
         with open(PHONEBOOK_ENTRIES, 'r') as f:
-            lookup = "".join(filter(lambda line: name in line, f.readlines()))
-            # YOUR CODE HERE #
-
+            for line in f.readlines():
+                entry = line.split(":")
+                if entry[0] == name:
+                    print line,
 
 if __name__ == "__main__":
     main()
